57d244b4b71010e6d169cdc9c382e513bbc9808e
laserboy3000 <93968311+laserboy3000@users.noreply.github.com>
2021-11-11 17:27:29 +0300

    Add files via upload

---
+++ Home_work.py
@@ -0,0 +1,15 @@
+print("Hello, Python!") # вывод приветствия 1
+print("Hello, Vladimir!") # вывод приветствия 2
+print()
+print("The itsy bitsy spider\nclimbed up the waterspout.") # перенос половины строки по команде \n
+print() # вывод пустой строки
+print("Down came the rain\nand washed the spider out.") # перенос половины строки по команде \n
+print()
+print("The itsy bitsy spider", "climbed up", "the waterspout.") # выведет все в одну строку через пробел
+print()
+print("My name is", "Python.", end=" ") # следующий принт будет выведен в эту же строку 
+print("Monty Python.") # следующий принт
+print()
+print("My", "name", "is", sep="_", end="*") # вывод с разделителем "_", окончание строки *, след строка выводится на это же
+print("Monty", "Python.", sep="*", end="*\n")
+
